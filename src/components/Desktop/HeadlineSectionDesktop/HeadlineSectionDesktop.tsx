7ff494f0c58aba94304f232865ca66528b34efde
kinqbert <perehodko1010@gmail.com>
2024-09-28 16:04:31 +0300

    headline section fixed

--- src/components/Desktop/HeadlineSectionDesktop/HeadlineSectionDesktop.tsx
+++ src/components/Desktop/HeadlineSectionDesktop/HeadlineSectionDesktop.tsx
@@ -2,14 +2,36 @@ import { OrderForm } from "~/components/common/OrderForm";
 import { PurchaseButton } from "~/components/common/PurchaseButton";
 
 import styles from "./HeadlineSectionDesktop.module.scss";
+import { useEffect, useState } from "react";
 
 export const HeadlineSectionDesktop = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const getOrderFormVariant = () => {
+    if (width < 1080) {
+      return "default";
+    }
+
+    return "large"
+  }
+
   return (
     <section className={styles.headlineSection}>
       <div className={styles.pageContentWrapper}>
         <div className={styles.pageContent}>
           <div className={styles.formWrapper}>
-            <OrderForm variant="large" />
+            <OrderForm variant={getOrderFormVariant()} />
           </div>
           <div className={styles.personBlock}>
             <h1 className={styles.personName}>СОФІЯ ПРОЗОРОВА</h1>
